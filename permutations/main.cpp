5a004ce7cb23111113abd69073ab18f2eb095678
Somnath Saha <somnathsaha.contact@gmail.com>
2020-04-05 02:41:12 +0530

    x

--- permutations/main.cpp
+++ permutations/main.cpp
@@ -4,23 +4,42 @@
 using namespace std;
 
 static int sno=0;
+static int sno2=0;
  
+void nCRPermutations(string s, int start, int r)
+{
+    if(start>=r) {
+        return;
+    }
+    cout << (++sno2) << ". " << s << endl;
+    nCRPermutations(s, start, r);
+}
+
 void printAllPermutations(string s, int start)
 {
-    if(start==(s.size()-1)) {
-        cout<<(++sno)<<". "<<s<<endl;
+    if (start >= s.size())
+    {
+        return;
     }
-    for(int i=start+1; i<s.size(); ++i) {
-        printAllPermutations(s, start+1);
-        std::swap(s[start], s[i]);
-        printAllPermutations(s, start+1);
-        std::swap(s[start], s[i]);
+    if (start == (s.size() - 1))
+    {
+        cout << (++sno) << ". " << s << endl;
+    }
+    else
+    {
+        printAllPermutations(s, start + 1);
+        for (int i = start + 1; i < s.size(); ++i)
+        {
+            std::swap(s[start], s[i]);
+            printAllPermutations(s, start + 1);
+            std::swap(s[start], s[i]);
+        }
     }
 }
 
 int main()
 {
-    string s = "abcd";
+    string s = "awqd";
     printAllPermutations(s, 0);
     return 0;
 }
