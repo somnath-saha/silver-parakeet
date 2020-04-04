5dfbc18a685bb9cfcaa007161a6761d69ff3e53d
Somnath Saha <somnathsaha.contact@gmail.com>
2020-04-05 01:59:28 +0530

    x

--- permutations/main.cpp
+++ permutations/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+static int sno=0;
+ 
+void printAllPermutations(string s, int start)
+{
+    if(start==(s.size()-1)) {
+        cout<<(++sno)<<". "<<s<<endl;
+    }
+    for(int i=start+1; i<s.size(); ++i) {
+        printAllPermutations(s, start+1);
+        std::swap(s[start], s[i]);
+        printAllPermutations(s, start+1);
+        std::swap(s[start], s[i]);
+    }
+}
+
 int main()
 {
-    cout<<"Hello World";
+    string s = "abcd";
+    printAllPermutations(s, 0);
     return 0;
 }
